50825d0284a91747eff3d9349e3387aa7b25d593
giovaneoliveirati <giovaneolioveira_ti@hotmail.com>
2024-10-15 06:34:45 -0300

    Configuracoes em geral do projeto.

--- src/main/java/com/vr/miniautorizador/MiniautorizadorApplication.java
+++ src/main/java/com/vr/miniautorizador/MiniautorizadorApplication.java
@@ -1,4 +1,4 @@
-package vr.miniautorizador;
+package com.vr.miniautorizador;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
